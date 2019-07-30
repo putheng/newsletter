a2b5ae433a2a5a28007f7f58743f2939de5c0dc2
Putheng <puthengemail@gmail.com>
2019-07-30 15:46:39 +0000

    upload

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,10 @@
 Route::post('login', 'Api\AuthController@login');
 
 Route::group(['middleware' => ['auth:api'], 'prefix' => 'v2'], function(){
-	Route::group(['middleware' => 'role:admin'], function () {
+	Route::get('me', 'AdminController@profile');
 
-		Route::get('me', 'AdminController@profile');
-
-		Route::post('news/delete', 'Admin\NewsController@destroy');
-		Route::post('news/create', 'Admin\NewsController@storeImage');
-
-	});
+	Route::post('news/delete', 'Admin\NewsController@destroy');
+	Route::post('news/create', 'Admin\NewsController@storeImage');
 });
 
 Route::get('/news', 'Admin\NewsController@show');
